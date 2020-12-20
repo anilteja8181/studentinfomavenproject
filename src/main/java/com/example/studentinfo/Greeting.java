b4afab47af382572be8e80e4a61cdd6490c3a322
anilteja8181 <75599291+anilteja8181@users.noreply.github.com>
2020-12-20 17:29:08 +0530

    Add files via upload

---
+++ src/main/java/com/example/studentinfo/Greeting.java
@@ -0,0 +1,34 @@
+package com.example.studentinfo;
+
+public class Greeting {
+
+	public int rollno;
+	public String name;
+	public String stream;
+	public String grade;
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStream() {
+		return stream;
+	}
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+}
